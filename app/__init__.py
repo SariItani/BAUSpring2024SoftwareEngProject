5058efa56af2f1eeb5b2335a9a22f1ab29b1b644
SariItani <74187768+SariItani@users.noreply.github.com>
2024-04-15 11:27:29 +0300

    made a racipe thingie

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,6 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
@@ -30,19 +29,31 @@ class User(UserMixin, db.Model):
         self.imgpath = imgpath
         self.bio = bio
 
-
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(500), nullable=False)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     sender = db.relationship('User', backref=db.backref('messages', lazy=True))
     message_type = db.Column(db.String(10), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=datetime.now)
 
     def __init__(self, content, sender, message_type):
         self.content = content
         self.sender = sender
         self.message_type = message_type
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    contributer_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    contributer = db.relationship('User', backref=db.backref('recipes', lazy=True))
+    rating = db.Column(db.Integer)
+    json_path = db.Column(db.String(100), nullable=False) # the data that may all exist or not exist, such as image path and what not
+
+    def __init__(self, json_path, contributer, rating):
+        self.json_path = json_path
+        self.contributer = contributer
+        self.rating = rating
 
 
 from app import routes
